34c595febea87edfaae06c25b27e955030692c2b
ahmadxp <ahmadnitro@proton.me>
2025-04-08 21:25:15 +0700

    fix some error

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 
 url = 'http://httpbin.org/ip'
 
-print("# PROXY CHECK\n# Github: https://github.com/ahmadxp/proxycheck\n")
+print("# PROXY CHECK\n# Github: https://github.com/ahmadxp/proxy-check\n")
 
 proxies = input('Input your proxy list: ')
 if not proxies.endswith('.txt'):
@@ -15,29 +15,31 @@ def check():
     bad = 0
     try:
         with open(proxies, 'r') as proxy:
-            os.system('clear')
+            os.system('cls' if os.name == 'nt' else 'clear')
             print("Start checking...")
             for proxy in proxy:
                 proxy = proxy.strip()
                 try:
                     # Send a GET request to the URL
                     response = requests.get(url, proxies={'http': proxy, 'https': proxy}, timeout=5)
-                    with open('result.txt', 'a') as f:
-                        if response.status_code == 200: # If the request was successful
-                            live += 1
-                            loc = requests.get(f'https://ipinfo.io/{response.json()['origin']}/json')
-                            if loc.status_code == 200:
-                                print(f"{Fore.GREEN}[LIVE - {loc.json()['country']}] {Fore.RESET}{proxy}") # Print the proxy and country
+                    if response.status_code == 200: # If the request was successful
+                        live += 1
+                        ip_origin = response.json()['origin']
+                        loc = requests.get(f'https://ipinfo.io/{ip_origin}/json')
+                        if loc.status_code == 200:
+                            print(f"{Fore.GREEN}[LIVE - {loc.json()['country']}] {Fore.RESET}{proxy}") # Print proxy and country
+                            with open('result.txt', 'a') as f:
                                 f.write(f"{proxy}\n")
-                            else:
-                                print(f"{Fore.GREEN}[LIVE] {Fore.RESET}{proxy}")
+                        else:
+                            print(f"{Fore.GREEN}[LIVE] {Fore.RESET}{proxy}")
+                            with open('result.txt', 'a') as f:
                                 f.write(f"{proxy}\n")
-                        elif response.status_code == 407:
-                            bad += 1
-                            print(f"{Fore.YELLOW}[Invalid Auth] {Fore.RESET}{proxy}") # 407 Proxy Authentication Required
-                        elif response.status_code == 429:
-                            bad += 1
-                            print(f"{Fore.YELLOW}[Too Many Requests] {Fore.RESET}{proxy}") # Too Many Requests
+                    elif response.status_code == 407:
+                        bad += 1
+                        print(f"{Fore.YELLOW}[Invalid Auth] {Fore.RESET}{proxy}") # 407 Proxy Authentication Required
+                    elif response.status_code == 429:
+                        bad += 1
+                        print(f"{Fore.YELLOW}[Too Many Requests] {Fore.RESET}{proxy}") # Too Many Requests
                 except requests.exceptions.ConnectionError:
                     bad += 1
                     print(f"{Fore.RED}[BAD] {Fore.RESET}{proxy}")  # Connection errors
